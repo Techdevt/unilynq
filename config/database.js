430e3abeb39fc6401686d3e9a2543df22b9cf231
fanky5g <appiahbrobbeybenjamin@rocketmail.com>
2014-09-04 21:30:52 +0000

    fourth commit

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-	url : 'mongodb://localhost:27017/unilynq'
+	url : 'mongodb://<fanky5g>:<fanky2010>@kahana.mongohq.com:10083/breezyapp'
 }
+//mongodb://<fanky5g>:<fanky2010>@kahana.mongohq.com:10083/breezyapp
+//mongodb://localhost:27017/unilynq
