3e0a8108c77fbd7e57fc215d6b4297e48ab0bd6e
fanky5g <appiahbrobbeybenjamin@rocketmail.com>
2014-09-04 21:50:35 +0000

    sixth commit

--- config/database.js
+++ config/database.js
@@ -2,4 +2,4 @@ module.exports = {
 	url : 'mongodb://<fanky5g>:<fanky2010>@kahana.mongohq.com:10083/breezyapp'
 }
 //mongodb://<fanky5g>:<fanky2010>@kahana.mongohq.com:10083/breezyapp
-//mongodb://localhost:27017/unilynq   i just deployed to github
+//mongodb://localhost:27017/unilynq 
