db28fdf53f772f6b882738dcec8a0385076c4d14
fanky5g <appiahbrobbeybenjamin@rocketmail.com>
2014-09-05 02:54:54 +0000

    commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	url : 'mongodb://<fanky5g>:<fanky2010>@kahana.mongohq.com:10083/breezyapp'
+	url : 'mongodb://fanky5g:fanky2010@kahana.mongohq.com:10083/breezyapp'
 }
 //mongodb://localhost:27017/unilynq 
